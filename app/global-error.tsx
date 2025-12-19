6edb6af40e8f347dae50d14cce443d6a8549b692
Shreyas Patel <shreyas.135@gmail.com>
2025-12-19 19:25:50 +0530

    Added example of global-error page

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,13 +1,10 @@
 "use client";
-
 export default function GlobalError({ error, reset }: { error: Error; reset: () => void }) {
     return (
-        <html>
-            <body>
-                <h2>Something went wrong! - 500 error</h2>
-                <p>{error.message}</p>
-                <button onClick={() => reset()}>Try again</button>
-            </body>
-        </html>
+        <>
+            <h2>Something went wrong! - 500 error</h2>
+            <p>{error.message}</p>
+            <button onClick={() => reset()}>Try again</button>
+        </>
     );
 }
